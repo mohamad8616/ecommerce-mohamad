bc640dfa8afa96fae365392aed0d2c7568960560
mohamad8616 <mohamad.sh.a9036@gmail.com>
2025-10-26 14:52:49 +0330

    discard one commit

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -5,24 +5,19 @@ export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const q = searchParams.get("q");
   const normalized = q?.normalize("NFC");
-  console.log(q, normalized);
+
   if (!q || q.length < 2) {
     return NextResponse.json({ results: [] });
   }
-  const fakeCount = await prisma.fakeProduct.count();
-  const dummyCount = await prisma.dummyProduct.count();
 
-  console.log("fake count:", fakeCount, "dummy count:", dummyCount);
   const fakeProducts = await prisma.fakeProduct.findMany({
     where: {
       category: { contains: normalized },
-      //   description: { contains: normalized },
     },
     take: 5,
   });
   const dummyProducts = await prisma.dummyProduct.findMany({
     where: {
-      //   description: { contains: normalized },
       category: { contains: normalized },
     },
 
